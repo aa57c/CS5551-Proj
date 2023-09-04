d6673ceca22cc5fbc81b9009816027897ba33b11
aa57c <aa57c@umsystem.edu>
2023-09-04 15:09:03 -0500

    initial commit for project

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is the start of our code. 
+from tkinter import *
+from tkinter.ttk import *
+window=Tk()
+
+sizeOfBoardLbl=Label(window, text="Size of Board:", foreground='red', font=("Helvetica", 16))
+txtfld=Entry(window, text="")
+txtfld.place(x=80, y=150)
+window.title('Nine Men\'s Morris')
+window.geometry("300x200+10+10")
+window.mainloop()
+
+
