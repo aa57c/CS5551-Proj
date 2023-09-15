e5a0c0c75409cfe283cdb0d50703d3d331979c85
aa57c <aa57c@umsystem.edu>
2023-09-15 11:13:13 -0500

    created new branch for placing a piece

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-# This is the start of our code. 
-from tkinter import *
-from tkinter.ttk import *
-window=Tk()
-
-sizeOfBoardLbl=Label(window, text="Size of Board:", foreground='red', font=("Helvetica", 16))
-txtfld=Entry(window, text="")
-txtfld.place(x=80, y=150)
-window.title('Nine Men\'s Morris')
-window.geometry("300x200+10+10")
-window.mainloop()
-
-print("hello world")
+# This is the start of placing a piece code (Ashna)
 
 
