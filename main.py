a8786db070cd3f57ff1f5deaf6e0fc64ab96fa99
aa57c <aa57c@umsystem.edu>
2023-09-20 21:15:27 -0500

    completed placing a piece user story. (THIS IS NOT FINAL)

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
 # This is the start of placing a piece code (Ashna)
+def print_board(board):
+    for row in board:
+        print(' | '.join(row))
+        print('-' * 21)
 
+def place_piece(board, player):
+    while True:
+        try:
+            position = input(f"Player {player}, enter the row and column (such as A1): ").strip().upper()
+            row, col = position[0], int(position[1]) - 1
+            if not('A' <= row <= 'G') or not (0 <= col <= 6) or board[ord(row) - ord('A')][col] != ' ':
+                raise ValueError("Invalid position. Try again.")
+            board[ord(row) - ord('A')][col] = player
+            break
+        except ValueError as e:
+            print(e)
+        except (IndexError, TypeError):
+            print("Invalid input format. Use the format 'A1'.")
+
+board = [[' ' for _ in range(7)] for _ in range(7)]
+
+player1 = 'X'
+player2 = 'O'
+players = [player1, player2]
+
+for _ in range(18):
+    current_player = players[_ % 2]
+    print_board(board)
+    place_piece(board, current_player)
 
